648a67a4dcfc796c496e27de8a242503257996d6
abdulazizax <abdulazizax2001@gmail.com>
2024-07-05 15:21:48 +0500

    main.go added 5.07.2024

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 CURRENT_DIR=$(shell pwd)
-DB_URL=postgres://postgres:pass@localhost:5432/greenthumb?sslmode=disable
+DB_URL=postgres://postgres:pass@localhost:5432/sustainability?sslmode=disable
 
 proto-gen:
 	./scripts/gen-proto.sh ${CURRENT_DIR}
--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,54 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"sustainabilityService/config"
+	pb "sustainabilityService/genproto/SustainabilityService"
+	"sustainabilityService/services"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+	"google.golang.org/grpc"
+)
+
+func GetDB(path string) (*sqlx.DB, error) {
+	cfg := config.Load(path)
+
+	psqlUrl := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		cfg.Postgres.DbHost,
+		cfg.Postgres.DbPort,
+		cfg.Postgres.DbUser,
+		cfg.Postgres.DbPassword,
+		cfg.Postgres.DbName,
+	)
+
+	db, err := sqlx.Connect("postgres", psqlUrl)
+	return db, err
+}
+
+func main() {
+	lis, err := net.Listen("tcp", "localhost:50053")
+	if err != nil {
+		log.Fatal("Failed to listen: ", err)
+	}
+
+	gprcServer := grpc.NewServer()
+
+	db, err := GetDB(".")
+	if err != nil {
+		log.Fatal("Failed to connect to database: ", err)
+	}
+
+	sustainabilityService, err := services.NewSustainabilityService(db, "50052")
+	if err != nil {
+		log.Fatal("Failed to create sustainability service: ", err)
+	}
+	pb.RegisterSustainabilityServiceServer(gprcServer, sustainabilityService)
+
+	log.Println("gRPC server is running on port 50053")
+	if err := gprcServer.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
+}
